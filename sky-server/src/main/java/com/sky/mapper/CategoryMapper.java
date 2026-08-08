2a521f3bbeb473447784f157bdde1528f7d7b336
lxey233 <2824642926@qq>
2026-08-08 18:48:57 +0800

    分类管理功能代码

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,51 @@
+package com.sky.mapper;
+
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 新增分类
+     *
+     * @param category
+     */
+    void insert(Category category);
+
+
+    /**
+     * 删除分类
+     *
+     * @param id
+     */
+    @Delete("delete from category where id = #{id}")
+    void deleteById(Long id);
+
+    /**
+     * 修改分类
+     *
+     * @param category
+     */
+    void update(Category category);
+
+    /**
+     * 分类分页查询
+     *
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    List<Category> page(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 根据类型查询分类
+     *
+     * @param type
+     * @return
+     */
+    List<Category> list(Integer type);
+}
