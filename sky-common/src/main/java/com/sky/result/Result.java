a81358e6c4b5d196d031355c08e4e55eb067be36
lxey233 <2824642926@qq>
2026-08-08 15:11:54 +0800

    新增员工代码开发

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -12,30 +12,27 @@ import java.io.Serializable;
 public class Result<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
-    private String msg;   //错误信息
-    private T data;       //数据
+    private String msg; //错误信息
+    private T data; //数据
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<T>();
         result.code = 1;
-        result.msg = "success";           // ← 加上非空提示
-        result.data = (T) new Object();   // ← 防止 data 为 null，序列化后为 {}
         return result;
     }
 
     public static <T> Result<T> success(T object) {
         Result<T> result = new Result<T>();
-        result.code = 1;
-        result.msg = "success";           // ← 加上提示
         result.data = object;
+        result.code = 1;
         return result;
     }
 
     public static <T> Result<T> error(String msg) {
-        Result<T> result = new Result<T>();
+        Result result = new Result();
         result.msg = msg;
         result.code = 0;
-        // data 可以为 null，错误时不返回数据
         return result;
     }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -81,9 +82,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
 
-        // TODO
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        BaseContext.removeCurrentId();
 
         employeeMapper.insert(employee);
     }
